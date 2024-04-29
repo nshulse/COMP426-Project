ea3df5c9d67ddb9e67df6adeb33807a60c32e4ab
Robert Wittmann <rwittmann@unc.edu>
2024-04-29 00:31:02 -0400

    database tables setup

--- db_setup.js
+++ db_setup.js
@@ -3,7 +3,10 @@ const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database('./db')
 
 // Creates table
-//db.run('CREATE TABLE accounts(id INT PRIMARY KEY,username,password)')
+//db.run('CREATE TABLE accounts(id INT PRIMARY KEY, username, password, saved_recipes)')
+
+//db.run('CREATE TABLE recipes (id INT PRIMARY KEY, FOREIGN KEY (author_id) REFERENCES accounts(id), sumamry, description, ingredients, amounts, instructions, times_saved INT, imageURL)');
+
 
 // Resets table
 //db.run('DELETE FROM accounts')
