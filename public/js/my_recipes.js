1e5ab9cf32b25f609b0b6ad8a8c448e458029a25
Noah Hulse <nhulse@unc.edu>
2024-04-30 00:49:40 -0400

    fix showing ingredients for my recipes (#7)

--- public/js/my_recipes.js
+++ public/js/my_recipes.js
@@ -3,23 +3,29 @@ const myRecipesListElement = document.getElementById('my-recipes-list');
 fetch('/my_recipes')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         data.forEach(recipe => {
-            console.log(recipe)
+            console.log(recipe.ingredients)
             const recipeElement = document.createElement('div');
             recipeElement.classList.add('recipe');
             recipeElement.classList.add('my-recipe')
-            let ing_text = [];
-            for(const ing of recipe.ingredients){
-                ing_text.push(ing.portion + ' ' + ing.unit + ' of ' + ing.name);
-            }
-            ing_text = ing_text.join("; ");
+            
+            let ingredients_text = []
+            let parsed = JSON.parse(recipe.ingredients)
+
+            parsed.forEach(ingredient => {
+                let name = ingredient.name
+                let portion = ingredient.portion
+                let unit = ingredient.unit
+                ingredients_text.push(' ' + portion + ' ' + unit + ' of ' + name + ' ')
+            });
+            console.log(parsed)
+
             recipeElement.innerHTML = `
                 <h2 style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">${recipe.title}</h2>
                 <p style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;"><strong>Summary:</strong> ${recipe.summary}</p>
                 <img src="${recipe.imageUrl}" alt="${recipe.title}" width = "300" />
                 <p style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;"><strong>Description:</strong> ${recipe.description}</p>
-                <p style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;"><strong>Ingredients:</strong> ${ing_text}</p>
+                <p style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;"><strong>Ingredients:</strong> ${ingredients_text}</p>
                 <p style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;"><strong>Instructions:</strong> ${recipe.instructions}</p>
             `;
             myRecipesListElement.appendChild(recipeElement);
