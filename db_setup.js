168f1d79e45f7673395643852a8d1a6ff827b011
Noah Hulse <nhulse@unc.edu>
2024-04-29 22:46:53 -0400

    fix database and add recipe (#6)
    * db setup and redirect to my_recipes after making one
    
    * progress on api and try to fix db (#5)
    
    ---------
    
    Co-authored-by: Robert Wittmann <rwittmann@unc.edu>

--- db_setup.js
+++ db_setup.js
@@ -5,10 +5,13 @@ const db = new sqlite3.Database('./db')
 // Creates tables
 db.run('CREATE TABLE accounts(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)')
 
-db.run('CREATE TABLE recipes (id INTEGER PRIMARY KEY, author_id INTEGER, summary TEXT, description TEXT, ingredients TEXT, amounts TEXT, instructions TEXT, times_saved INTEGER, imageURL TEXT, FOREIGN KEY (author_id) REFERENCES accounts(id))');
+db.run('CREATE TABLE recipes (id INTEGER PRIMARY KEY AUTOINCREMENT, author_id INTEGER, title TEXT summary TEXT, description TEXT, ingredients TEXT, amounts TEXT, instructions TEXT, times_saved INTEGER, imageURL TEXT, FOREIGN KEY (author_id) REFERENCES accounts(id))');
 
 db.run('CREATE TABLE saved_recipes(id INTEGER PRIMARY KEY, user_id INTEGER, recipe_id INTEGER, FOREIGN KEY (user_id) REFERENCES accounts(id), FOREIGN KEY (recipe_id) REFERENCES recipes(id))');
 
 
 // Resets table
-//db.run('DELETE FROM accounts')
+//db.run('DELETE accounts')
+
+// Deletes table
+//db.run("DROP TABLE recipes")
